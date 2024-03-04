4baa1a3aedd3a18a336e8efb98ec7c14fab87b60
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-03-05 00:44:06 +0100

    feature : add carousel + first mobile design (media queries)

--- projects/playground/src/app/carousel/carousel.component.ts
+++ projects/playground/src/app/carousel/carousel.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrl: './carousel.component.scss',
 })
 export class CarouselComponent {
+  @Input('slides')
+  slides: any[] = [];
 
+  currentSlide = 0;
+  hidden = false;
+  next() {
+    let currentSlide = (this.currentSlide + 1) % this.slides.length;
+    this.jumpToSlide(currentSlide);
+  }
+
+  previous() {
+    let currentSlide =
+      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    this.jumpToSlide(currentSlide);
+  }
+
+  jumpToSlide(index: number) {
+    this.hidden = true;
+    setTimeout(() => {
+      this.currentSlide = index;
+      this.hidden = false;
+    }, 250);
+  }
 }
--- projects/playground/src/app/product-img/product-img.component.ts
+++ projects/playground/src/app/product-img/product-img.component.ts
@@ -1,10 +1,11 @@
 import { Component, HostBinding, Input } from '@angular/core';
+import { CarouselComponent } from '../carousel/carousel.component';
 import { routeAnimationState } from '../shared/route-animation';
 
 @Component({
   selector: 'section[product-images]',
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './product-img.component.html',
   styleUrl: './product-img.component.scss',
   animations: [routeAnimationState],
@@ -13,4 +14,22 @@ export class ProductImgComponent {
   @HostBinding('@routeAnimationTrigger') routeAnimation = true;
   @Input('title')
   title: string = '';
+
+  slides: any[] = [
+    {
+      url: './assets/img/reality.jpg',
+    },
+    {
+      url: './assets/img/up.jpg',
+    },
+    {
+      url: './assets/img/face2.jpg',
+    },
+    {
+      url: './assets/img/connect.jpg',
+    },
+    {
+      url: './assets/img/details.jpg',
+    },
+  ];
 }
